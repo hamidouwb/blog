c40c4813bd0d3cc4359cca72e784b2c7cd16b715
Hamidou Ballo <ballowara@gmail.com>
2025-10-28 02:27:04 -0500

    Remove Vercel build cache from repo and update .gitignore

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.ClGVvbm_.js","_app/immutable/chunks/scheduler.qK7VSXAp.js","_app/immutable/chunks/index.CYcFyMQE.js","_app/immutable/chunks/stores.Cpe8Dsff.js","_app/immutable/chunks/entry.BJbsb8ZX.js","_app/immutable/chunks/index.DDu_y74h.js"];
-export const stylesheets = [];
-export const fonts = [];
