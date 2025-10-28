666d0e5b6b0798db5f805f4f3fe535d8ee08531c
Hamidou Ballo <ballowara@gmail.com>
2025-10-28 01:37:29 -0500

    headshot, added some depth to 3d model

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,7 +38,10 @@
     justify-content: flex-start; /* Justifies content to the left */
     align-items: center;
     border-radius: 300px;
-    box-shadow: inset 0 0 0 1px hsla(0, 0%, 0%, 1);
+    box-shadow:
+      inset 0 0 0 1px hsla(0, 0%, 0%, 0.2),
+      0 10px 25px -5px rgba(0, 0, 0, 0.3),
+      0 8px 10px -6px rgba(0, 0, 0, 0.2);
     margin-top: 10px
   }
 </style>
